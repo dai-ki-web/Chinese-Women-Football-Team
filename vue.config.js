8f594831ab121e5ef1ff132bcd8e927d4a2ed3c1
dai-ki-web <70361565+dai-ki-web@users.noreply.github.com>
2022-07-26 09:41:55 +0800

    change config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+
+const path = require('path')
+module.exports = {
+  publicPath: './',
+  outputDir: 'dist',
   transpileDependencies: true,
-})
+  devServer: {
+    // 调试端口
+    port: 8989
+  },
+
+}
