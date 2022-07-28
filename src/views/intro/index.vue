6d4d20b5a90d6ebc2bcdb43bda79a0c77e2d886e
Counting-China-Women-Football <Women_Football@163.com>
2022-07-28 12:34:41 +0800

    修改正文颜色&添加交互图片

--- src/views/intro/index.vue
+++ src/views/intro/index.vue
@@ -13,15 +13,15 @@
     </div>
     <section class="section">
       <p>2022年2月6日，时隔十六年重返亚洲杯决赛的中国女足，在印度新孟买的DY Patil体育场与韩国女足展开较量。中国队在落后两球的不利情况下连扳三球，最终3∶2逆转击败韩国队。
-        十六载蛰伏，一举问鼎亚洲之巅，中国女足收获了历史上第九个亚洲杯冠军。
+        十六载蛰伏，一举问鼎亚洲之巅，中国女足收获了<span class="red">历史上第九个亚洲杯冠军</span>。
         女足姑娘们一点一滴的汗水化作露珠，浇灌着铿锵玫瑰再一次绽放。</p>
-      <p>在本场比赛中，中国队多项数据占优：全场近60%的控球率，14次射门比对手韩国队多出3次， 6次射正也同样领先对手。</p>
+      <p>在本场比赛中，中国队<span class="red">多项数据占优</span>：全场近60%的控球率，14次射门比对手韩国队多出3次， 6次射正也同样领先对手。</p>
     </section>
     <div class="image">
       <el-image :src="img7" fit="contain" lazy></el-image>
     </div>
     <section class="section">
-      <p>这场女足亚洲杯决赛的收视率数据同样亮眼：中韩大战的收视率创历史新高地达到了2.44%。肖裕仪完成绝杀的巅峰时刻，共3745万人同时观看，收视率高达2.2%，排名全国第一。中国女足在本次大赛中得到了前所未有的关注。</p>
+      <p>这场女足亚洲杯决赛的<span class="red">收视率数据同样亮眼</span>：中韩大战的收视率创历史新高地达到了2.44%。肖裕仪完成绝杀的巅峰时刻，共3745万人同时观看，收视率高达2.2%，排名全国第一。中国女足在本次大赛中得到了前所未有的关注。</p>
     </section>
     <!-- 电视 -->
     <div class="image">
@@ -46,7 +46,7 @@
         img6: require('@/assets/images/6.jpeg'),
         img7: require('@/assets/images/7.jpeg'),
         img8: require('@/assets/images/8.jpg'),
-        img10: require('@/assets/images/10.jpeg')
+        img10: require('@/assets/images/10.png')
       }
     }
   }
