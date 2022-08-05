13a0ffe0a51c070a630724bf8e652b0551a42eff
Counting-China-Women-Football <Women_Football@163.com>
2022-08-05 19:32:46 +0800

    部分内容修改

--- src/views/problem/index.vue
+++ src/views/problem/index.vue
@@ -43,11 +43,9 @@
       </div>
       <section class="section">
         <p>中国女足国家队现役球员（共23人）中，来自武汉车谷江大女足和上海农商银行女足<span class="red">2支队伍的球员占比约70%（共15人）</span>。相比之下，其他省市俱乐部被征召的球员凤毛麟角。</p>
-      </section>
-      <iframe src='https://flo.uri.sh/visualisation/10751158/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/10751158/?utm_source=embed&utm_campaign=visualisation/10751158' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
-      <section class="section">
         <p>中国女足国家队<span class="red">选才范围小且难度大</span>，国内联赛俱乐部<span class="red">难以长期、稳定地为国家队输送优秀球员</span>。在未来，该困境所导致的<span class="red">人才断层问题</span>，将影响周期性大赛成绩和国家队持续发展。</p>
       </section>
+      <iframe src='https://flo.uri.sh/visualisation/10751158/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/10751158/?utm_source=embed&utm_campaign=visualisation/10751158' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
       <el-divider></el-divider>
     </div>
     <!-- b -->
@@ -59,11 +57,9 @@
       <section class="section">
         <p>中国女足国家队现役球员（共23人）中，有<span class="red">11人</span>（均在2022年女足亚洲杯淘汰赛中首发上场，为国家队主力球员）<span class="red">在近3年内进行过俱乐部转会</span>。</p>
         <p>目前国内女足顶级联赛（中国女足超级联赛）中，<span class="red">60%的俱乐部建队时间不超过5年，70%的俱乐部在5年内更换过主赞助商。</span></p>
-      </section>
-      <iframe src="https://hanabi.data-viz.cn/chart.html?id=s_55c9287faf2144409272db89fc29cbc5&isIframe=1" frameborder="0" width="100%" height="960px"></iframe>
-      <section class="section">
         <p>中国女足国家队球员在国内联赛中<span class="red">流动频繁</span>，其所在<span class="red">俱乐部状态不稳定且环境欠佳</span>。这将直接影响国家队球员在<span class="red">日常比赛和训练中的出场时间和竞技状态</span>，很难实现快速成长以提升国家队整体实力。</p>
       </section>
+       <iframe src="https://hanabi.data-viz.cn/chart.html?id=s_55c9287faf2144409272db89fc29cbc5&isIframe=1" frameborder="0" width="100%" height="960px"></iframe>
       <el-divider></el-divider>
     </div>
     <!-- c -->
@@ -74,13 +70,11 @@
       </div>
       <section class="section">
         <p>近5年，中国女足关注度整体小于中国男足，<span class="red">女足赛事热度骤升骤降</span>的情况较为普遍。女足球迷的<span class="red">性别分布差异大</span>（在全网用户的性别分布为女性50.32%，男性49.68%的基础上，关注中国女足用户性别分布为女性30.96%，男性69.04%） 且<span class="red">地域分布不均衡</span>（主要来自北京、上海等一线城市，其他省市球迷较少）。</p>
-      </section>
-      <iframe src='https://flo.uri.sh/visualisation/10749513/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/10749513/?utm_source=embed&utm_campaign=visualisation/10749513' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
-      <section class="section">
         <p>
           <span class="red">观赛动机弱</span>和<span class="red">关注度不稳定不均衡</span>的状态，是我国女足球迷<span class="red">忠诚度和持续性差</span>的体现。在女足赛事本身观赏度不高，女足俱乐部品牌文化欠缺的背景下，女足国家队和国内联赛难以建立<span class="red">忠实的球迷群体和稳固的群众基础</span>。这将导致女足<span class="red">发展地位和资源配置</span>长期处于男足从属地位，影响中国女足长足发展。
         </p>
       </section>
+      <iframe src='https://flo.uri.sh/visualisation/10749513/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/10749513/?utm_source=embed&utm_campaign=visualisation/10749513' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
       <el-divider></el-divider>
     </div>
     <div class="liner-end">
